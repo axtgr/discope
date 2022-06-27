49a93c87e0144d99c1b0c23bd9f2e0f848e90826
Alex Schneider <me@schneider.ax>
2022-06-27 20:13:55 +0700

    It works

---
+++ src/graph.ts
@@ -0,0 +1,77 @@
+interface GraphNode<TValue = any> {
+  children: GraphNode[]
+  parent?: GraphNode
+  value?: TValue
+}
+
+class GraphBuilder {
+  isBuilding = false
+  nodeMap?: Map<any, GraphNode>
+  root?: GraphNode
+  currentNode?: GraphNode
+
+  start() {
+    if (this.isBuilding) {
+      throw new Error('Cannot start building a graph that is already being built')
+    }
+
+    this.nodeMap = new Map()
+    this.root = { children: [] }
+    this.currentNode = this.root
+    this.isBuilding = true
+    return this.root!
+  }
+
+  addNode(key: unknown, valueInitializer: () => unknown) {
+    if (!this.isBuilding) {
+      throw new Error('Cannot create a node when not building a graph')
+    }
+
+    if (!this.nodeMap!.has(key)) {
+      this.nodeMap!.set(key, { parent: this.currentNode, children: [] })
+    }
+
+    const node = this.nodeMap!.get(key) as GraphNode
+    this.currentNode!.children.push(node)
+    this.currentNode = node
+    node.value = valueInitializer()
+    this.currentNode = node.parent
+    return node
+  }
+
+  finish() {
+    if (!this.isBuilding) {
+      throw new Error("Cannot finish building a graph that isn't being built")
+    }
+
+    const result = this.root!
+    this.nodeMap = undefined
+    this.root = undefined
+    this.currentNode = undefined
+    this.isBuilding = false
+    return result
+  }
+}
+
+function traverseFromLeaves(
+  node: GraphNode,
+  visitor: (node: GraphNode) => unknown,
+  visits: Map<GraphNode, unknown> = new Map()
+) {
+  let visit = visits.get(node)
+  if (!visit) {
+    visit = Promise.resolve()
+      .then(() => {
+        const childVisits = node.children.map((child) => {
+          return traverseFromLeaves(child, visitor, visits)
+        })
+        return Promise.all(childVisits)
+      })
+      .then(() => visitor(node))
+    visits.set(node, visit)
+  }
+  return visit
+}
+
+export type { GraphNode }
+export { GraphBuilder, traverseFromLeaves }
---
+++ src/index.ts
@@ -0,0 +1,139 @@
+import { GraphNode, GraphBuilder, traverseFromLeaves } from './graph.js'
+
+type ResolvedDependencies = Record<string, unknown>
+
+type DependencyResolver<TArgs extends any[] = unknown[], TDependency = unknown> = (
+  ...args: TArgs
+) => TDependency
+
+type Dependency<
+  TArgs extends any[] = unknown[],
+  TDependency = unknown
+> = DependencyResolver<TArgs, TDependency>
+
+type DependencyMap = Record<string, Dependency>
+
+type UnresolveDependencies<TResolvedDependencies extends ResolvedDependencies> = {
+  [K in keyof TResolvedDependencies]: Dependency<unknown[], TResolvedDependencies[K]>
+}
+
+type ResolveDependencies<TResolvers extends DependencyMap> = {
+  [K in keyof TResolvers]: TResolvers[K] extends Dependency<any, infer R> ? R : never
+}
+
+type Dependencies<TResolvedDependencies extends ResolvedDependencies> =
+  UnresolveDependencies<TResolvedDependencies> & (() => TResolvedDependencies)
+
+type ScopeResolver<
+  TExports extends DependencyMap,
+  TResolvedDependencies extends ResolvedDependencies | void = void
+> = TResolvedDependencies extends ResolvedDependencies
+  ? (deps: Dependencies<TResolvedDependencies>) => TExports
+  : () => TExports
+
+type Scope<
+  TExports extends DependencyMap | unknown = unknown,
+  TResolvedDependencies extends ResolvedDependencies | void = void
+> = TResolvedDependencies extends ResolvedDependencies
+  ? (deps: UnresolveDependencies<TResolvedDependencies>) => TExports
+  : () => TExports
+
+const graphBuilder = new GraphBuilder()
+
+function single<TArgs extends any[], TResolvedDependency>(
+  resolver: DependencyResolver<TArgs, TResolvedDependency>
+) {
+  let executed = false
+  let result: ReturnType<DependencyResolver>
+  return ((...args) => {
+    const node = graphBuilder.addNode(resolver, () => {
+      if (!executed) {
+        result = resolver(...args)
+        executed = true
+      }
+      return result
+    })
+    return node.value
+  }) as Dependency<TArgs, TResolvedDependency>
+}
+
+function factory<TArgs extends any[], TResolvedDependency>(
+  resolver: DependencyResolver<TArgs, TResolvedDependency>
+) {
+  return ((...args) => {
+    const node = graphBuilder.addNode(Symbol(), () => resolver(...args))
+    return node.value
+  }) as Dependency<TArgs, TResolvedDependency>
+}
+
+function resolveDependencies(dependencies: DependencyMap | Dependency) {
+  if (typeof dependencies === 'function') {
+    return dependencies()
+  }
+
+  return Object.keys(dependencies).reduce((result, key) => {
+    result[key] = resolveDependencies(dependencies[key])
+    return result
+  }, {} as ResolvedDependencies)
+}
+
+function createDepsFn<TResolvedDependencies extends ResolvedDependencies>(
+  deps: UnresolveDependencies<TResolvedDependencies>
+): Dependencies<TResolvedDependencies> {
+  const fn = (() => resolveDependencies(deps)) as Dependencies<TResolvedDependencies>
+  Object.assign(fn, deps)
+  return fn
+}
+
+function scope<
+  TExports extends DependencyMap,
+  TResolvedDependencies extends ResolvedDependencies | void = void
+>(resolver: ScopeResolver<TExports, TResolvedDependencies>) {
+  return ((deps) => {
+    const node = graphBuilder.addNode(Symbol(), () => {
+      return resolver((deps && createDepsFn(deps)) as any)
+    })
+    return node.value
+  }) as Scope<TExports, TResolvedDependencies>
+}
+
+/**
+ * Given a root scope, builds a graph of the scopes and returns its root node.
+ */
+function buildScopes<TExports extends DependencyMap>(rootScope: Scope<TExports, any>) {
+  graphBuilder.start()
+  rootScope({})
+  return graphBuilder.finish().children[0] as GraphNode<ResolveDependencies<TExports>>
+}
+
+/**
+ * Given the root node of a scope graph (returned by buildScopes()),
+ * resolves the dependencies and returns the root node of the graph.
+ */
+function buildDependencies(rootScopeNode: GraphNode<any>) {
+  graphBuilder.start()
+  resolveDependencies(rootScopeNode.value)
+  return graphBuilder.finish()
+}
+
+function build<TExports extends DependencyMap>(rootScope: Scope<any, TExports>) {
+  const rootScopesNode = buildScopes(rootScope)
+  const rootDependenciesNode = buildDependencies(rootScopesNode)
+  return rootDependenciesNode.value
+}
+
+function resolve<TExports extends DependencyMap>(rootScope: Scope<any, TExports>) {
+  return build(rootScope).value
+}
+
+export {
+  scope,
+  single,
+  factory,
+  build,
+  resolve,
+  buildScopes,
+  buildDependencies,
+  traverseFromLeaves,
+}
+export type { Scope, Dependency, Dependencies, GraphNode }
